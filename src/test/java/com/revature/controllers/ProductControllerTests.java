65f435313c61c01e7982586bed4ebc18eb3f3e0e
Fylo4 <93356384+Fylo4@users.noreply.github.com>
2022-10-18 03:05:06 -0400

    Added test cases to product controller

--- src/test/java/com/revature/controllers/ProductControllerTests.java
+++ src/test/java/com/revature/controllers/ProductControllerTests.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,28 +33,6 @@ public class ProductControllerTests {
     public MockMvc mvc;
     
     public static int createdProductId = 0;
-
-	//Can't get the session to persist
-	/*@BeforeTestClass
-	@Mock
-	static MockHttpSession session;
-    
-	public void setup() {
-		try {
-
-			session = new MockHttpSession();
-	        ResultActions auth = mvc.perform(MockMvcRequestBuilders.post("/auth/login").contentType(APPLICATION_JSON_UTF8)
-	                .content("{\"email\": \"testuser@gmail.com\", \"password\": \"password\", \"role\": \"User\"}")
-	                .session(session));
-	        MvcResult mvcResult = auth.andReturn();
-	        session = (MockHttpSession) mvcResult.getRequest().getSession();
-	        
-		} catch(Exception e) {
-			System.out.println("Exception thrown while trying to initialize exception");
-			System.out.println("Exception stack trace:");
-			e.printStackTrace();
-		}
-	}*/
 	
 	private MockHttpSession getSession() throws Exception {
 
@@ -67,71 +46,130 @@ public class ProductControllerTests {
 	}
 	
     @Test
-    @Order(1)
     public void viewProductsTest() throws Exception
     {
         mvc.perform(MockMvcRequestBuilders.get("/api/product/").session(getSession()))
         	.andExpect(status().isOk())
-        	.andExpect(content().string(org.hamcrest.Matchers.containsString("\"productId\":4,\"name\":\"Baseball Cap\",\"description\":\"A fancy baseball cap for a fancy person\"")));
+        	.andExpect(content().string(org.hamcrest.Matchers.containsString("\"name\":\"Tennis Ball Chew Toy\",\"description\":\"Heavy duty and made with durable materials so it will last a long time\"")));
     }
     
     @Test
-    @Order(2)
     public void searchNoResultsTest() throws Exception
     {
         mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
         	.param("name", "sgstdfklskjdnfkk"))
     		.andExpect(status().isOk())
     		.andExpect(content().string(equalTo("[]")));
-        
     }
-    
+
     @Test
-    @Order(3)
-    public void searchSuccessTest() throws Exception
+    public void searchByNameTest() throws Exception
     {
         String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
-        	.param("name", "bag")
+        	.param("name", "chew")
         	.param("order_by", "name"))
     		.andExpect(status().isOk())
             .andReturn().getResponse().getContentAsString();
-
-        String[] resultSplit = result.split("[:,]");
-        Assertions.assertEquals("\"Garbage Bags\"", resultSplit[3]);
         
+        System.out.println("Product controller by name");
+        System.out.println(result);
     }
     
     @Test
-    @Order(4)
+    public void searchByPriceTest() throws Exception
+    {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
+        	.param("name", "chew")
+        	.param("order_by", "price"))
+    		.andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        
+        System.out.println("Product controller by price");
+        System.out.println(result);
+    }
+    
+    @Test
+    public void searchByPriceDefaultTest() throws Exception
+    {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
+        	.param("name", "chew")
+        	.param("order_by", "geariushg"))
+    		.andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        
+        System.out.println("Product controller by default");
+        System.out.println(result);
+    }
+
+    @Test
+    public void searchByReviewTest() throws Exception
+    {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
+        	.param("name", "chew")
+        	.param("order_by", "review"))
+    		.andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        
+        System.out.println("Product controller by review");
+        System.out.println(result);
+    }
+
+    @Test
+    public void searchByCategoryTest() throws Exception
+    {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
+        	.param("name", "chew")
+        	.param("order_by", "category"))
+    		.andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        
+        System.out.println("Product controller by category");
+        System.out.println(result);
+    }
+
+    @Test
+    public void searchByQuantityTest() throws Exception
+    {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/api/product/search").session(getSession())
+        	.param("name", "chew")
+        	.param("order_by", "quantity"))
+    		.andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        
+        System.out.println("Product controller by quantity");
+        System.out.println(result);
+    }
+    
+    @Test
     public void getByIdTest() throws Exception{
         mvc.perform(MockMvcRequestBuilders.get("/api/product/2").session(getSession()))
     		.andExpect(status().isOk())
-    		.andExpect(content().string(org.hamcrest.Matchers.containsString("\"productId\":2,\"name\":\"TeeShirt\",\"description\":\"A nice TeeShirt\"")));
+    		.andExpect(content().string(org.hamcrest.Matchers.containsString("Lamb Flavored Dry Dog Food")));
     }
     
-    /*@Test
-    @Order(5)
+    //@Test
+    @Order(1)
     public void putProductTest() throws Exception{
         String result = mvc.perform(MockMvcRequestBuilders.put("/api/product").session(getSession())
         	.contentType(APPLICATION_JSON_UTF8)
-        	.content("{\"name\":\"TestProduct\",\"description\":\"Testing functionality of uploading and deleting products\",\"quantity\":10,\"price\":50,\"image\":\"\",\"rating\":3,\"review_count\":5,\"category_id\":1}"))
+        	.content("{'name':'TestProduct','description':'Testing functionality of uploading and deleting products','quantity':10,'price':50,'image':'','rating':3,'review_count':5,'category_id':1}"))
         	.andExpect(status().is(201))
         	.andReturn().getResponse().getContentAsString();
         System.out.println("Test 5 result: "+result);
         //Set createdProductId here based on what exactly is returned
     }
 
-    @Test
-    @Order(6)
+    //@Test
+    @Order(2)
     public void deleteProductTest() throws Exception{
         mvc.perform(MockMvcRequestBuilders.delete("/api/product/"+createdProductId).session(getSession()))
     		.andExpect(status().isOk());
     }
     
-    @Test
-    @Order(7)
+    //@Test
+    @Order(3)
     public void deleteProductFailedTest() throws Exception{
         mvc.perform(MockMvcRequestBuilders.delete("/api/product/"+createdProductId).session(getSession()))
     		.andExpect(status().is(404));
-    }*/
+    }
 }
